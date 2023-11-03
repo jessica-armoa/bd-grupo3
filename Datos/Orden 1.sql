ca237a62e140b53dc38fbdffc8dd79e49d965622
jhpkn22 <manujuanmelgarejo@gmail.com>
2023-11-03 15:38:02 -0300

    Inserciones orden 1 y 2

--- Datos/Orden 1.sql
+++ Datos/Orden 1.sql
@@ -1,3 +1,13 @@
 /************************************/
+/*		TABLA DE PERSONAS			*/
+insert into personas (persona id,nombre,direccion,telefono,correo_electronico) values (1,
+
+
+
+/***********************************/
+
+
+
+/************************************/
 /*         TABLA DEPOSITOS          */
 /************************************/
--- Datos/Orden 2.sql
+++ Datos/Orden 2.sql
@@ -1,3 +1,29 @@
 /************************************/
 /*         TABLA PRODUCTOS          */
 /************************************/
+
+/************************************/
+/*         TABLA CLIENTES          */
+
+insert into clientes (cliente_id, persona_id, ruc) values (1,1,4965564)
+insert into clientes (cliente_id, persona_id, ruc) values (2,2,4965565)
+insert into clientes (cliente_id, persona_id, ruc) values (3,3,4965566)
+insert into clientes (cliente_id, persona_id, ruc) values (4,4,4965567)
+insert into clientes (cliente_id, persona_id, ruc) values (5,5,4965568)
+insert into clientes (cliente_id, persona_id, ruc) values (6,6,4965569)
+
+/************************************/
+
+/************************************/
+/*         TABLA EMPLEADOS          */
+/************************************/
+insert into empleado (empleado_id, persona_id, cargo) values (1,7,'encargados')
+insert into empleado (empleado_id, persona_id, cargo) values (2,8,'autorizantes')
+insert into empleado (empleado_id, persona_id, cargo) values (3,9,'encargados')
+insert into empleado (empleado_id, persona_id, cargo) values (4,10,'autorizantes')
+
+
+/************************************/
+/*         TABLA PRODUCTOS          */
+/************************************/
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (1,7,1,'Elvive Hidra Hialurónico 200ml',0,23000, 0.1);
