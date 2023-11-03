7b3eee6bfcfd23a41c637d96cd56bf14a5a998c7
jhpkn22 <manujuanmelgarejo@gmail.com>
2023-11-03 15:43:02 -0300

    datos 2

--- Datos/Orden 1.sql
+++ Datos/Orden 1.sql
@@ -1,13 +1,49 @@
 /************************************/
 /*		TABLA DE PERSONAS			*/
-insert into personas (persona id,nombre,direccion,telefono,correo_electronico) values (1,
-
-
-
 /***********************************/
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (1, 'Juan', 'Av.Caballero', 0993294588, 'manujuan@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (2, 'Pedro', 'Juan Leon', 0993294589, 'manupedro@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (3, 'Jessica','Av.Caballero', 0985791322, 'jessica@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (4, 'Juto', 'Av. Jose Felix Bogado', 0983538533, 'jutoram@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (5, 'Manuel', 'Pedro Juan Caballero', 0993297588, 'manujuan@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (6, 'Miguel', 'Av.Caballero', 0984322625, 'miguel@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (7, 'Belen', 'Av.Eusebio', 0793294588, 'belen@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (8, 'Dionsio', 'Artigas', 0993294788, 'dionisio@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (9, 'Cristhian', 'Av. Columbo', 0943294588, 'cristhian@gmail.com')
+insert into personas (persona_id, nombre, direccion, telefono, correo_electronico) values (10, 'Nico', 'Av. Encarnacion', 0985147635, 'niconi@gmail.com')
 
 
 
 /************************************/
 /*         TABLA DEPOSITOS          */
 /************************************/
+insert into depositos (deposito_id, tipo_deposito) values(1,'Deposito General');
+insert into depositos (deposito_id, tipo_deposito) values(2,'Deposito Salon');
+
+
+/************************************/
+/*         TABLA MARCAS          */
+/************************************/
+
+insert into marcas (marca_id, marca) values(1,'Schwarzkopf');
+insert into marcas(marca_id, marca) values(2, 'Saules');
+insert into marcas (marca_id, marca) values(3, 'Vitu');
+insert into marcas (marca_id, marca) values(4, 'Vogue');
+insert into marcas (marca_id, marca) values(5, 'Hinode');
+insert into marcas (marca_id, marca) values(6, 'Wella');
+insert into marcas (marca_id, marca) values(7, 'Loreal Paris');
+insert into marcas (marca_id, marca) values(8, 'Servicio');
+
+/************************************/
+/*         TABLA TIPOS         */
+/************************************/
+insert into tipos (tipo_id, tipo) values(1,'Shampoo');
+insert into tipos (tipo_id, tipo) values(2,'Acondicionador');
+insert into tipos (tipo_id, tipo) values(3,'Tinte');
+insert into tipos (tipo_id, tipo) values(4,'Hidratacion');
+insert into tipos (tipo_id, tipo) values(5,'Crema facial');
+insert into tipos (tipo_id, tipo) values(6,'Baño de crema');
+insert into tipos (tipo_id, tipo) values(7,'Corte');
+
+
+
--- Datos/Orden 2.sql
+++ Datos/Orden 2.sql
@@ -1,10 +1,6 @@
 /************************************/
-/*         TABLA PRODUCTOS          */
-/************************************/
-
-/************************************/
 /*         TABLA CLIENTES          */
-
+/************************************/
 insert into clientes (cliente_id, persona_id, ruc) values (1,1,4965564)
 insert into clientes (cliente_id, persona_id, ruc) values (2,2,4965565)
 insert into clientes (cliente_id, persona_id, ruc) values (3,3,4965566)
@@ -12,7 +8,7 @@ insert into clientes (cliente_id, persona_id, ruc) values (4,4,4965567)
 insert into clientes (cliente_id, persona_id, ruc) values (5,5,4965568)
 insert into clientes (cliente_id, persona_id, ruc) values (6,6,4965569)
 
-/************************************/
+
 
 /************************************/
 /*         TABLA EMPLEADOS          */
@@ -27,3 +23,15 @@ insert into empleado (empleado_id, persona_id, cargo) values (4,10,'autorizantes
 /*         TABLA PRODUCTOS          */
 /************************************/
 insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (1,7,1,'Elvive Hidra Hialurónico 200ml',0,23000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (2,7,2,'Elvive Hidra Hialurónico 200ml',0,23000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (3,7,1,'Elvive Hidra Hialurónico 370ml',0,42000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (4,7,2,'Elvive Hidra Hialurónico 370ml',0,42000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (5,1,1,'Reparador R-TWO clean performance 250ml',0, 80000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (11,1,2,'Reparador R-TWO clean performance 250ml',0, 80000, 0.1);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (6,1,6,'Baño crema hidratacion 200ml',0, 80000, 0.1);
+
+
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (7,8,4,'Hidratacion profunda',1, 50000, 0.05);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (8,8,7,'Corte femenino largo 1',1, 45000, 0.05);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (9,8,7,'Corte femenino largo 2',1, 60000, 0.05);
+insert into productos (producto_id, marca_id, tipo_id,descripcion,es_servicio,precio_unitario,iva) values (10,8,7,'Corte masculino',1, 60000, 0.05);
