4a24422271484c6a71ac827d7b1988b1ac11c09d
jhpkn22 <manujuanmelgarejo@gmail.com>
2023-11-23 01:15:20 -0300

    gola

--- triggers1.sql
+++ triggers1.sql
@@ -18,7 +18,7 @@ AS
 update stocks set stocks.cantidad_producto = stocks.cantidad_producto + inserted.cantidad from inserted
 inner join transferencias t on t.TRANFERENCIA_ID = inserted.TRANFERENCIA_ID
 inner join stocks on stocks.deposito_id = t.DEPOSITO_DESTINO_ID
-
+/*
 select * from transferencias
 select *from detalles_transferencias
 
@@ -31,10 +31,12 @@ values
 
 insert into detalles_transferencias (tranferencia_id, producto_id, cantidad) 
 values (1,14,1)
+*/
 go
 
-/*------------------TRIGGER QUE ACTUALIZA EL STOCK AL REALIZAR UN SERVICIO------------------*/
-CREATE TRIGGER TR_STOCKS_DPU
+
+/*------------------TRIGGER QUE ACTUALIZA EL STOCK AL INSERTAR UN SERVICIO------------------*/
+CREATE TRIGGER TR_INSERT_DPU
 ON detalles_productos_utilizados
 AFTER INSERT
 AS
@@ -48,7 +50,43 @@ BEGIN
     INNER JOIN inserted i ON stocks.producto_id = i.producto_id;
 END
 
+*------------------TRIGGER QUE ACTUALIZA EL STOCK AL ELIMINAR UN SERVICIO------------------*/
 
+GO
+CREATE TRIGGER TR_DELETE_DPU
+ON detalles_productos_utilizados
+AFTER DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Actualizar la cantidad de productos en la tabla stocks después de eliminar un detalle
+    UPDATE stocks
+    SET cantidad_producto = stocks.cantidad_producto + d.cantidad
+    FROM stocks
+    INNER JOIN deleted d ON stocks.producto_id = d.producto_id;
+END;
+
+*------------------TRIGGER QUE ACTUALIZA EL STOCK AL ACTULIZAR UN SERVICIO------------------*/
+
+go
+CREATE TRIGGER TR_UPDATE_DPU
+ON detalles_productos_utilizados
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Actualizar la cantidad de productos en la tabla stocks después de una actualización
+    UPDATE s
+    SET s.cantidad_producto = s.cantidad_producto + (i.cantidad - d.cantidad)
+    FROM stocks s
+    INNER JOIN deleted d ON s.producto_id = d.producto_id
+    INNER JOIN inserted i ON s.producto_id = i.producto_id
+    WHERE d.cantidad IS NOT NULL AND i.cantidad IS NOT NULL;
+END;
+
+/*
 select * from stocks ;
 
 insert into servicios_realizados
@@ -70,11 +108,11 @@ insert into DETALLES_PRODUCTOS_UTILIZADOS
   (PRODUCTO_ID, SERVICIO_REALIZADO_ID, CANTIDAD, PRECIO_UNITARIO, IVA)
 values
   (14, 2, 1, 110000, 0.1); -- Tinte miel
-
-/*------------------TRIGGER QUE ACTUALIZA FACTURADO O NO------------------ Listo*/
+  */
+/*------------------TRIGGER QUE ACTUALIZA FACTURADO A 1 CUANDO SE CREA UNA FACTURA------------------ Listo*/
 
 GO
-CREATE TRIGGER after_insert_factura
+CREATE TRIGGER TR_INSERT_FACTURA
 ON facturas
 AFTER INSERT
 AS
@@ -89,3 +127,18 @@ BEGIN
     WHERE sr.facturado = 0; -- Asegurar que el servicio no haya sido facturado previamente
 END;
 
+/*------------------TRIGGER QUE ACTUALIZA FACTURADO A 0 CUANDO SE ELIMINA UNA FACTURA ------------------ Listo*/
+go
+CREATE TRIGGER TR_DELETE_FACTURA
+ON facturas
+AFTER DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Desmarcar como facturado en la tabla servicios_realizados después de eliminar una factura
+    UPDATE sr
+    SET sr.facturado = 0
+    FROM servicios_realizados sr
+    INNER JOIN deleted d ON sr.servicio_realizado_id = d.servicio_realizado_id;
+END;
